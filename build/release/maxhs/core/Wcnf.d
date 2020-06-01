955b398ea92c03e10d1219939775eb9d0d6548a3
Randy Grant Hickey <rhickey@mail.cs.toronto.edu>
2020-06-01 15:42:12 -0400

    init

---
+++ build/release/maxhs/core/Wcnf.d
@@ -0,0 +1,17 @@
+build/release/maxhs/core/Wcnf.o: maxhs/core/Wcnf.cc \
+ minisat/utils/System.h minisat/mtl/IntTypes.h minisat/core/SolverTypes.h \
+ minisat/mtl/Alg.h minisat/mtl/Vec.h minisat/mtl/XAlloc.h \
+ minisat/mtl/IntMap.h minisat/mtl/Map.h minisat/mtl/Alloc.h \
+ maxhs/core/Wcnf.h maxhs/ds/Packed.h maxhs/core/MaxSolverTypes.h \
+ maxhs/ifaces/miniSatSolver.h minisat/simp/SimpSolver.h \
+ minisat/mtl/Queue.h minisat/core/Solver.h minisat/mtl/Heap.h \
+ minisat/utils/Options.h minisat/utils/ParseUtils.h \
+ maxhs/ifaces/SatSolver.h maxhs/utils/io.h maxhs/core/Dimacs.h \
+ maxhs/core/MaxSolver.h maxhs/ifaces/GreedySolver.h maxhs/core/Bvars.h \
+ maxhs/ifaces/muser.h maxhs/ifaces/Cplex.h \
+ /w/63/fbacchus/CPLEX_Studio1210/cplex/include/ilcplex/cplexx.h \
+ /w/63/fbacchus/CPLEX_Studio1210/cplex/include/ilcplex/cpxconst.h \
+ /w/63/fbacchus/CPLEX_Studio1210/cplex/include/ilcplex/cplexl.h \
+ maxhs/utils/Params.h maxhs/core/Assumptions.h satlike/basis_pms.h \
+ satlike/../maxhs/core/MaxSolver.h satlike/../maxhs/core/Wcnf.h \
+ maxhs/utils/hash.h
